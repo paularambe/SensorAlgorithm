ff07f896f39a429bb74c7915190b0b56560b369d
Paul Aramberri <paramberria@alumni.unav.es>
2024-12-17 18:50:51 +0000

    Cam

---
+++ Server/bb.sh
@@ -0,0 +1,5 @@
+sudo apt-get update
+sudo apt-get install -y build-essential cmake git pkg-config \
+libjpeg-dev libtiff-dev libpng-dev libavcodec-dev libavformat-dev libswscale-dev \
+libv4l-dev libxvidcore-dev libx264-dev libgtk-3-dev libcanberra-gtk3-module \
+libatlas-base-dev gfortran python3-dev
